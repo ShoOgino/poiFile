ShoOgino/poiFile

commit 5a5bbba7b6fb11026d7f3897c032feb14fbda37b

Added Apache License text to files where it was missing. Also fixed some compiler warnings in those files.
git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@745976 13f79535-47bb-0310-9956-ffa450edef68

src/java/org/apache/poi/ddf/EscherContainerRecord.java
@@ -43,7 +43,7 @@ public class EscherContainerRecord extends EscherRecord
     public static final short SP_CONTAINER     = (short)0xF004;
     public static final short SOLVER_CONTAINER = (short)0xF005;
 
-    private List childRecords = new ArrayList();
+    private final List<EscherRecord> _childRecords = new ArrayList<EscherRecord>();
 
     public int fillFields( byte[] data, int offset, EscherRecordFactory recordFactory )
     {
@@ -110,7 +110,7 @@ public class EscherContainerRecord extends EscherRecord
         {
             EscherRecord r = (EscherRecord) iterator.next();
             if(r.getRecordId() == recordId) {
-            	return true;
+                return true;
             }
         }
         return false;
@@ -120,9 +120,8 @@ public class EscherContainerRecord extends EscherRecord
      * Returns a list of all the child (escher) records
      *  of the container.
      */
-    public List getChildRecords()
-    {
-        return childRecords;
+    public List<EscherRecord> getChildRecords() {
+        return _childRecords;
     }
     
     /**
@@ -131,7 +130,7 @@ public class EscherContainerRecord extends EscherRecord
      *   2 or 3)
      */
     public List getChildContainers() {
-    	List containers = new ArrayList();
+        List containers = new ArrayList();
         for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
         {
             EscherRecord r = (EscherRecord) iterator.next();
@@ -142,15 +141,13 @@ public class EscherContainerRecord extends EscherRecord
         return containers;
     }
 
-    public void setChildRecords( List childRecords )
-    {
-        this.childRecords = childRecords;
+    public void setChildRecords(List<EscherRecord> childRecords) {
+        _childRecords.clear();
+        _childRecords.addAll(childRecords);
     }
 
-    public String getRecordName()
-    {
-        switch ((short)getRecordId())
-        {
+    public String getRecordName() {
+        switch (getRecordId()) {
             case DGG_CONTAINER:
                 return "DggContainer";
             case BSTORE_CONTAINER:
@@ -171,16 +168,15 @@ public class EscherContainerRecord extends EscherRecord
     public void display( PrintWriter w, int indent )
     {
         super.display( w, indent );
-        for ( Iterator iterator = childRecords.iterator(); iterator.hasNext(); )
+        for (Iterator iterator = _childRecords.iterator(); iterator.hasNext();)
         {
             EscherRecord escherRecord = (EscherRecord) iterator.next();
             escherRecord.display( w, indent + 1 );
         }
     }
 
-    public void addChildRecord( EscherRecord record )
-    {
-        this.childRecords.add( record );
+    public void addChildRecord(EscherRecord record) {
+        _childRecords.add( record );
     }
 
     public String toString()
@@ -226,7 +222,7 @@ public class EscherContainerRecord extends EscherRecord
 
     public EscherSpRecord getChildById( short recordId )
     {
-        for ( Iterator iterator = childRecords.iterator(); iterator.hasNext(); )
+        for ( Iterator iterator = _childRecords.iterator(); iterator.hasNext(); )
         {
             EscherRecord escherRecord = (EscherRecord) iterator.next();
             if (escherRecord.getRecordId() == recordId)
@@ -241,7 +237,7 @@ public class EscherContainerRecord extends EscherRecord
      * @param out - list to store found records
      */
     public void getRecordsById(short recordId, List out){
-        for(Iterator it = childRecords.iterator(); it.hasNext();) {
+        for(Iterator it = _childRecords.iterator(); it.hasNext();) {
             Object er = it.next();
             EscherRecord r = (EscherRecord)er;
             if(r instanceof EscherContainerRecord) {

src/scratchpad/src/org/apache/poi/hslf/model/MovieShape.java
@@ -1,20 +1,37 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
 package org.apache.poi.hslf.model;
 
-import org.apache.poi.ddf.*;
-import org.apache.poi.hslf.record.*;
-import org.apache.poi.hslf.exceptions.HSLFException;
-import org.apache.poi.hslf.usermodel.SlideShow;
-import org.apache.poi.util.LittleEndian;
-
 import java.io.ByteArrayOutputStream;
-import java.util.Iterator;
+
+import org.apache.poi.ddf.EscherClientDataRecord;
+import org.apache.poi.ddf.EscherContainerRecord;
+import org.apache.poi.ddf.EscherProperties;
+import org.apache.poi.hslf.exceptions.HSLFException;
+import org.apache.poi.hslf.record.*;
+import org.apache.poi.hslf.usermodel.SlideShow;
 
 /**
  * Represents a movie in a PowerPoint document.
  *
  * @author Yegor Kozlov
  */
-public class MovieShape extends Picture {
+public final class MovieShape extends Picture {
     public static final int DEFAULT_MOVIE_THUMBNAIL = -1;
 
     public static final int MOVIE_MPEG = 1;

src/java/org/apache/poi/ddf/EscherRecord.java
@@ -30,16 +30,15 @@ import java.util.List;
  *
  * @author Glen Stampoultzis
  */
-abstract public class EscherRecord
-{
-    private short options;
-    private short recordId;
+public abstract class EscherRecord {
+    private short _options;
+    private short _recordId;
 
     /**
      * Create a new instance
      */
-    public EscherRecord()
-    {
+    public EscherRecord() {
+        // fields uninitialised
     }
 
     /**
@@ -73,11 +72,10 @@ abstract public class EscherRecord
      * @return          the number of bytes remaining in this record.  This
      *                  may include the children if this is a container.
      */
-    protected int readHeader( byte[] data, int offset )
-    {
+    protected int readHeader( byte[] data, int offset ) {
         EscherRecordHeader header = EscherRecordHeader.readHeader(data, offset);
-        options = header.getOptions();
-        recordId = header.getRecordId();
+        _options = header.getOptions();
+        _recordId = header.getRecordId();
         return header.getRemainingBytes();
     }
 
@@ -86,26 +84,23 @@ abstract public class EscherRecord
      * field.
      * @return  true is this is a container field.
      */
-    public boolean isContainerRecord()
-    {
-        return (options & (short)0x000f) == (short)0x000f;
+    public boolean isContainerRecord() {
+        return (_options & (short)0x000f) == (short)0x000f;
     }
 
     /**
      * @return The options field for this record.  All records have one.
      */
-    public short getOptions()
-    {
-        return options;
+    public short getOptions() {
+        return _options;
     }
 
     /**
      * Set the options this this record.  Container records should have the
      * last nibble set to 0xF.
      */
-    public void setOptions( short options )
-    {
-        this.options = options;
+    public void setOptions( short options ) {
+        _options = options;
     }
 
     /**
@@ -164,17 +159,15 @@ abstract public class EscherRecord
      *
      * @return  The 16 bit record id.
      */
-    public short getRecordId()
-    {
-        return recordId;
+    public short getRecordId() {
+        return _recordId;
     }
 
     /**
      * Sets the record id for this record.
      */
-    public void setRecordId( short recordId )
-    {
-        this.recordId = recordId;
+    public void setRecordId( short recordId ) {
+        _recordId = recordId;
     }
 
     /**
@@ -184,7 +177,7 @@ abstract public class EscherRecord
      *
      * @see EscherContainerRecord
      */
-    public List getChildRecords() { return Collections.EMPTY_LIST; }
+    public List<EscherRecord> getChildRecords() { return Collections.emptyList(); }
 
     /**
      * Sets the child records for this record.  By default this will throw
@@ -192,7 +185,9 @@ abstract public class EscherRecord
      *
      * @param childRecords  Not used in base implementation.
      */
-    public void setChildRecords( List childRecords ) { throw new IllegalArgumentException("This record does not support child records."); }
+    public void setChildRecords(List<EscherRecord> childRecords) {
+        throw new UnsupportedOperationException("This record does not support child records.");
+    }
 
     /**
      * Escher records may need to be clonable in the future.
@@ -205,9 +200,8 @@ abstract public class EscherRecord
     /**
      * Returns the indexed child record.
      */
-    public EscherRecord getChild( int index )
-    {
-        return (EscherRecord) getChildRecords().get(index);
+    public EscherRecord getChild( int index ) {
+        return getChildRecords().get(index);
     }
 
     /**
@@ -233,9 +227,8 @@ abstract public class EscherRecord
      *
      * @return The instance part of the record
      */
-    public short getInstance()
-    {
-        return (short) ( options >> 4 );
+    public short getInstance() {
+        return (short) ( _options >> 4 );
     }
 
     /**
@@ -247,8 +240,8 @@ abstract public class EscherRecord
         private short recordId;
         private int remainingBytes;
 
-        private EscherRecordHeader()
-        {
+        private EscherRecordHeader() {
+            // fields uninitialised
         }
 
         public static EscherRecordHeader readHeader( byte[] data, int offset )
@@ -284,8 +277,5 @@ abstract public class EscherRecord
                     ", remainingBytes=" + remainingBytes +
                     "}";
         }
-
-
     }
-
 }

src/java/org/apache/poi/hssf/record/formula/eval/RefEvalBase.java
@@ -1,5 +1,27 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
 package org.apache.poi.hssf.record.formula.eval;
 
+/**
+ * Common base class for implementors of {@link RefEval}
+ *
+ * @author Josh Micich
+ */
 public abstract class RefEvalBase implements RefEval {
 
 	private final int _rowIndex;

src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFActiveXData.java
@@ -1,3 +1,20 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
 package org.apache.poi.xssf.usermodel;
 
 import java.io.IOException;
@@ -9,43 +26,47 @@ import org.apache.poi.xssf.model.XSSFWritableModel;
 import org.apache.poi.POIXMLException;
 import org.apache.poi.openxml4j.opc.PackagePart;
 
-public class XSSFActiveXData implements PictureData, XSSFWritableModel {
+/**
+ * 
+ * @author Nick Burch
+ */
+public final class XSSFActiveXData implements PictureData, XSSFWritableModel {
 
-    private PackagePart packagePart;
-    private String originalId;
+	private final PackagePart _packagePart;
+	private final String _originalId;
 
-    public XSSFActiveXData(PackagePart packagePart, String originalId) {
-        this(packagePart);
-        this.originalId = originalId;
-    }
-    
-    public XSSFActiveXData(PackagePart packagePart) {
-        this.packagePart = packagePart;
-    }
-
-    public String getOriginalId() {
-    	return originalId;
-    }
-    
-    public PackagePart getPart() {
-    	return packagePart;
-    }
-    
-	public void writeTo(OutputStream out) throws IOException {
-		IOUtils.copy(packagePart.getInputStream(), out);
+	public XSSFActiveXData(PackagePart packagePart, String originalId) {
+		_packagePart = packagePart;
+		_originalId = originalId;
 	}
 
-    public byte[] getData() {
-    	// TODO - is this right?
-    	// Are there headers etc?
-    	try {
-    		return IOUtils.toByteArray(packagePart.getInputStream());
-    	} catch(IOException e) {
-    		throw new POIXMLException(e);
-    	}
-    }
+	public XSSFActiveXData(PackagePart packagePart) {
+		this(packagePart, null);
+	}
 
-    public String suggestFileExtension() {
-    	return packagePart.getPartName().getExtension();
-    }
+	public String getOriginalId() {
+		return _originalId;
+	}
+
+	public PackagePart getPart() {
+		return _packagePart;
+	}
+
+	public void writeTo(OutputStream out) throws IOException {
+		IOUtils.copy(_packagePart.getInputStream(), out);
+	}
+
+	public byte[] getData() {
+		// TODO - is this right?
+		// Are there headers etc?
+		try {
+			return IOUtils.toByteArray(_packagePart.getInputStream());
+		} catch(IOException e) {
+			throw new POIXMLException(e);
+		}
+	}
+
+	public String suggestFileExtension() {
+		return _packagePart.getPartName().getExtension();
+	}
 }

commit 0091339cb96501c17c112446e32e157ebb957e20

Fix for bug introduced in r745976.  EscherContainerRecord shouldn't hand out it's private 'child records' field.
git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@746018 13f79535-47bb-0310-9956-ffa450edef68

src/java/org/apache/poi/ddf/EscherContainerRecord.java
@@ -34,8 +34,7 @@ import java.io.PrintWriter;
  *
  * @author Glen Stampoultzis
  */
-public class EscherContainerRecord extends EscherRecord
-{
+public final class EscherContainerRecord extends EscherRecord {
     public static final short DGG_CONTAINER    = (short)0xF000;
     public static final short BSTORE_CONTAINER = (short)0xF001;
     public static final short DG_CONTAINER     = (short)0xF002;
@@ -57,7 +56,7 @@ public class EscherContainerRecord extends EscherRecord
             bytesWritten += childBytesWritten;
             offset += childBytesWritten;
             bytesRemaining -= childBytesWritten;
-            getChildRecords().add( child );
+            addChildRecord(child);
             if (offset >= data.length && bytesRemaining > 0)
             {
                 System.out.println("WARNING: " + bytesRemaining + " bytes remaining but no space left");
@@ -73,16 +72,16 @@ public class EscherContainerRecord extends EscherRecord
         LittleEndian.putShort(data, offset, getOptions());
         LittleEndian.putShort(data, offset+2, getRecordId());
         int remainingBytes = 0;
-        for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
-        {
-            EscherRecord r = (EscherRecord) iterator.next();
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             remainingBytes += r.getRecordSize();
         }
         LittleEndian.putInt(data, offset+4, remainingBytes);
         int pos = offset+8;
-        for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
-        {
-            EscherRecord r = (EscherRecord) iterator.next();
+        iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             pos += r.serialize(pos, data, listener );
         }
 
@@ -90,12 +89,11 @@ public class EscherContainerRecord extends EscherRecord
         return pos - offset;
     }
 
-    public int getRecordSize()
-    {
+    public int getRecordSize() {
         int childRecordsSize = 0;
-        for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
-        {
-            EscherRecord r = (EscherRecord) iterator.next();
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             childRecordsSize += r.getRecordSize();
         }
         return 8 + childRecordsSize;
@@ -106,44 +104,49 @@ public class EscherContainerRecord extends EscherRecord
      *  given recordId?
      */
     public boolean hasChildOfType(short recordId) {
-        for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
-        {
-            EscherRecord r = (EscherRecord) iterator.next();
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             if(r.getRecordId() == recordId) {
                 return true;
             }
         }
         return false;
     }
-
+    
     /**
-     * Returns a list of all the child (escher) records
-     *  of the container.
+     * @return a copy of the list of all the child records of the container.
      */
     public List<EscherRecord> getChildRecords() {
-        return _childRecords;
+        return new ArrayList<EscherRecord>(_childRecords);
     }
-    
+    /**
+     * replaces the internal child list with the contents of the supplied <tt>childRecords</tt>
+     */
+    public void setChildRecords(List<EscherRecord> childRecords) {
+    	if (childRecords == _childRecords) {
+    		throw new IllegalStateException("Child records private data member has escaped");
+    	}
+        _childRecords.clear();
+        _childRecords.addAll(childRecords);
+    }
+
+   
     /**
      * Returns all of our children which are also
      *  EscherContainers (may be 0, 1, or vary rarely
      *   2 or 3)
      */
-    public List getChildContainers() {
-        List containers = new ArrayList();
-        for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
-        {
-            EscherRecord r = (EscherRecord) iterator.next();
+    public List<EscherContainerRecord> getChildContainers() {
+        List<EscherContainerRecord> containers = new ArrayList<EscherContainerRecord>();
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             if(r instanceof EscherContainerRecord) {
-            	containers.add(r);
+            	containers.add((EscherContainerRecord) r);
             }
         }
         return containers;
-    }
-
-    public void setChildRecords(List<EscherRecord> childRecords) {
-        _childRecords.clear();
-        _childRecords.addAll(childRecords);
     }
 
     public String getRecordName() {
@@ -165,13 +168,12 @@ public class EscherContainerRecord extends EscherRecord
         }
     }
 
-    public void display( PrintWriter w, int indent )
-    {
-        super.display( w, indent );
-        for (Iterator iterator = _childRecords.iterator(); iterator.hasNext();)
+    public void display(PrintWriter w, int indent) {
+        super.display(w, indent);
+        for (Iterator<EscherRecord> iterator = _childRecords.iterator(); iterator.hasNext();)
         {
-            EscherRecord escherRecord = (EscherRecord) iterator.next();
-            escherRecord.display( w, indent + 1 );
+            EscherRecord escherRecord = iterator.next();
+            escherRecord.display(w, indent + 1);
         }
     }
 
@@ -188,16 +190,15 @@ public class EscherContainerRecord extends EscherRecord
         String nl = System.getProperty( "line.separator" );
 
         StringBuffer children = new StringBuffer();
-        if ( getChildRecords().size() > 0 )
-        {
+        if (_childRecords.size() > 0) {
             children.append( "  children: " + nl );
             
             int count = 0;
-            for ( Iterator iterator = getChildRecords().iterator(); iterator.hasNext(); )
+            for ( Iterator<EscherRecord> iterator = _childRecords.iterator(); iterator.hasNext(); )
             {
             	String newIndent = indent + "   ";
             	
-                EscherRecord record = (EscherRecord) iterator.next();
+                EscherRecord record = iterator.next();
                 children.append(newIndent + "Child " + count + ":" + nl);
                 
                 if(record instanceof EscherContainerRecord) {
@@ -215,18 +216,17 @@ public class EscherContainerRecord extends EscherRecord
             indent + "  isContainer: " + isContainerRecord() + nl +
             indent + "  options: 0x" + HexDump.toHex( getOptions() ) + nl +
             indent + "  recordId: 0x" + HexDump.toHex( getRecordId() ) + nl +
-            indent + "  numchildren: " + getChildRecords().size() + nl +
+            indent + "  numchildren: " + _childRecords.size() + nl +
             indent + children.toString();
 
     }
 
-    public EscherSpRecord getChildById( short recordId )
-    {
-        for ( Iterator iterator = _childRecords.iterator(); iterator.hasNext(); )
-        {
-            EscherRecord escherRecord = (EscherRecord) iterator.next();
-            if (escherRecord.getRecordId() == recordId)
-                return (EscherSpRecord) escherRecord;
+    public EscherSpRecord getChildById(short recordId) {
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
+            if (r.getRecordId() == recordId)
+                return (EscherSpRecord) r;
         }
         return null;
     }
@@ -236,15 +236,15 @@ public class EscherContainerRecord extends EscherRecord
      *
      * @param out - list to store found records
      */
-    public void getRecordsById(short recordId, List out){
-        for(Iterator it = _childRecords.iterator(); it.hasNext();) {
-            Object er = it.next();
-            EscherRecord r = (EscherRecord)er;
+    public void getRecordsById(short recordId, List<EscherRecord> out){
+        Iterator<EscherRecord> iterator = _childRecords.iterator();
+        while (iterator.hasNext()) {
+            EscherRecord r = iterator.next();
             if(r instanceof EscherContainerRecord) {
                 EscherContainerRecord c = (EscherContainerRecord)r;
                 c.getRecordsById(recordId, out );
             } else if (r.getRecordId() == recordId){
-                out.add(er);
+                out.add(r);
             }
         }
     }

src/scratchpad/src/org/apache/poi/hslf/model/MovieShape.java
@@ -83,7 +83,7 @@ public final class MovieShape extends Picture {
 
         EscherClientDataRecord cldata = new EscherClientDataRecord();
         cldata.setOptions((short)0xF);
-        _escherContainer.getChildRecords().add(cldata);
+        _escherContainer.addChildRecord(cldata);
 
         OEShapeAtom oe = new OEShapeAtom();
         InteractiveInfo info = new InteractiveInfo();

src/testcases/org/apache/poi/ddf/TestEscherContainerRecord.java
@@ -1,4 +1,3 @@
-
 /* ====================================================================
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
@@ -20,14 +19,17 @@ package org.apache.poi.ddf;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.List;
 
 import junit.framework.TestCase;
 import org.apache.poi.util.HexRead;
 import org.apache.poi.util.HexDump;
 import org.apache.poi.util.IOUtils;
 
-public class TestEscherContainerRecord extends TestCase
-{
+/**
+ * Tests for {@link EscherContainerRecord}
+ */
+public final class TestEscherContainerRecord extends TestCase {
 	private String ESCHER_DATA_PATH;
 	
 	protected void setUp() {
@@ -129,17 +131,18 @@ public class TestEscherContainerRecord extends TestCase
                    "  properties:" + nl;
         assertEquals( expected, r.toString() );
     }
+    
+    private static final class DummyEscherRecord extends EscherRecord {
+    	public DummyEscherRecord() { }
+        public int fillFields( byte[] data, int offset, EscherRecordFactory recordFactory ) { return 0; }
+        public int serialize( int offset, byte[] data, EscherSerializationListener listener ) { return 0; }
+        public int getRecordSize() { return 10; }
+        public String getRecordName() { return ""; }
+    }
 
     public void testGetRecordSize() {
         EscherContainerRecord r = new EscherContainerRecord();
-        r.addChildRecord(new EscherRecord()
-        {
-            public int fillFields( byte[] data, int offset, EscherRecordFactory recordFactory ) { return 0; }
-            public int serialize( int offset, byte[] data, EscherSerializationListener listener ) { return 0; }
-            public int getRecordSize() { return 10; }
-            public String getRecordName() { return ""; }
-        } );
-
+        r.addChildRecord(new DummyEscherRecord());
         assertEquals(18, r.getRecordSize());
     }
 
@@ -158,4 +161,36 @@ public class TestEscherContainerRecord extends TestCase
     	EscherContainerRecord record = new EscherContainerRecord();
     	record.fillFields(data, 0, new DefaultEscherRecordFactory());
     }
+    
+    /**
+     * Ensure {@link EscherContainerRecord} doesn't spill its guts everywhere
+     */
+    public void testChildren() {
+    	EscherContainerRecord ecr = new EscherContainerRecord();
+    	List<EscherRecord> children0 = ecr.getChildRecords();
+    	assertEquals(0, children0.size());
+
+    	EscherRecord chA = new DummyEscherRecord();
+    	EscherRecord chB = new DummyEscherRecord();
+    	EscherRecord chC = new DummyEscherRecord();
+    	
+    	ecr.addChildRecord(chA);
+    	ecr.addChildRecord(chB);
+    	children0.add(chC);
+    	
+    	List<EscherRecord> children1 = ecr.getChildRecords();
+    	assertTrue(children0 !=  children1);
+    	assertEquals(2, children1.size());
+    	assertEquals(chA, children1.get(0));
+    	assertEquals(chB, children1.get(1));
+    	
+    	assertEquals(1, children0.size()); // first copy unchanged
+    	
+    	ecr.setChildRecords(children0);
+    	ecr.addChildRecord(chA);
+    	List<EscherRecord> children2 = ecr.getChildRecords();
+    	assertEquals(2, children2.size());
+    	assertEquals(chC, children2.get(0));
+    	assertEquals(chA, children2.get(1));
+	}
 }